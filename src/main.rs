309c3b7e3587be72272816dd832554ff0b1dded7
Yesterday17 <mmf@mmf.moe>
2023-04-11 09:49:48 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
-use std::{net::SocketAddr, process::Command};
+use std::{
+    net::SocketAddr,
+    process::{Command, Stdio},
+    thread::sleep,
+    time::Duration,
+};
 
 use axum::{routing::post, Router};
 
-async fn open(body: String) -> String {
-    let mut command = Command::new("xdg-open");
-    command.arg(body);
-    match command.status() {
-        Ok(status) => {
-            if status.success() {
-                format!("success")
-            } else {
-                format!("code: {:?}", status.code())
-            }
-        }
-        Err(e) => format!("error: {e}"),
-    }
+async fn open(body: String) -> Result<(), String> {
+    eprintln!("body: {}", body);
+    let mut command = Command::new("xdg-open")
+        .arg(body)
+        .stdout(Stdio::null())
+        .spawn()
+        .map_err(|e| format!("failed to spawn: {e:?}"))?;
+
+    sleep(Duration::from_secs(5));
+    let _ = command.kill();
+
+    Ok(())
 }
 
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", post(open));
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], 6655));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 6000));
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
